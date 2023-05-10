ab0d0fdef19202ae32db659b5ba111f41e88ebdc
CAkeysama <gustavodomingues2004gghz@gmail.com>
2023-05-10 10:57:46 -0300

    copia a e luan

--- WepApp_manha/Controllers/CategoriaController.cs
+++ WepApp_manha/Controllers/CategoriaController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WepApp_manha.Entidades;
+using WepApp_manha.Models;
 
 namespace WepApp_manha.Controllers
 {
@@ -12,7 +14,58 @@ namespace WepApp_manha.Controllers
         }
         public IActionResult Lista()
         {
-            return View(db.CATEGORIA.ToList());
+            return View(contexto.CATEGORIA.ToList());
+        }
+
+        public IActionResult Cadastro() 
+        { 
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SalvarDados(CategoriaViewModel dados)
+        {
+            CategoriaEntidade entidade = new CategoriaEntidade();
+            entidade.Nome = dados.Nome;
+            entidade.Id = dados.Id;
+            entidade.Ativo = dados.Ativo;
+
+            entidade.Descricao = dados.Descricao;
+            if(entidade.Id > 0 ) 
+            {
+                contexto.CATEGORIA.Update(entidade);
+                contexto.SaveChanges();
+            }
+            else
+            {
+                contexto.CATEGORIA.Add(entidade);
+                contexto.SaveChanges();
+            }
+           
+            return RedirectToAction("Lista");
+        }
+
+        public IActionResult Excluir(int id) 
+        {
+            CategoriaEntidade item = contexto.CATEGORIA.Find(id);
+            if (item != null) 
+            {
+                contexto.CATEGORIA.Remove(item);
+                contexto.SaveChanges();
+            }
+
+            return RedirectToAction("Lista");
+        }
+
+        public IActionResult Editar(int id)
+        {
+            CategoriaEntidade item = contexto.CATEGORIA.Find(id);
+            if (item != null) 
+            {
+                return View(item);
+            }else{
+                return RedirectToAction("Lista");
+            }
         }
     }
 }
---
+++ WepApp_manha/Models/CategoriaViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WepApp_manha.Models
+{
+    public class CategoriaViewModel
+    {
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
